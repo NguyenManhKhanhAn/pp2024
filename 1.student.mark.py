ff47af1a58cd35020f328ec94890539670484891
NguyenManhKhanhAn <annmk.23bi14003@usth.edu.vn>
2024-12-04 16:30:09 +0700

    rewrote the code to use functions

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,51 +1,68 @@
-StudentNum= int(input("Number of Students: "))
+def AddStudent(StudentNum, Students):
+	for i in range(StudentNum):
+		print(f"\nStudent {i+1}: ")
+
+		StudentId=int(input("Id: "))
+		StudentName=input("Name: ")
+		StudentDoB=input("Date of Birth: ")
+
+		Students.append([StudentId,StudentName,StudentDoB])
+
+def AddCourse(CourseNum, Courses, CourseDict):
+	for i in range(CourseNum):
+		print(f"\nCourse {i+1}: ")
+
+		CourseId=int(input("Id: "))
+		CourseName=input("Name: ")
+
+		Courses.append([CourseId,CourseName])
+		CourseDict.update({str(CourseName):[]})
+
+def SelectCourse(StudentNum, CourseName, Students, CourseDict):
+	for i in range(StudentNum):
+		print(f"\nSelect Course for Student {i+1}: ")
+
+		CourseName=input("Name: ")
+		CourseMark=int(input("Mark: "))
+		CourseDict[CourseName]+=[Students[i][0],Students[i][1],Students[i][2],CourseMark]	
+		#Key: CourseName, Value: Students[i] (Id, Name, DoB, Mark)
+
+def DisplayList(List, name):
+	print(f"\nList of {name}: ")
+
+	if name=="Students": 
+		print("ID\tName\tDoB")
+	if name=="Courses":
+		print("ID\tName")
+
+	for row in List:
+		for element in row:
+			print(element,end="\t")
+		print("")
+
+def DisplayStudentInCourses(CourseNum, Courses, CourseDict):
+	for i in range (CourseNum):
+		CourseName = Courses[i][1]
+		print(f"\nCourse '{CourseName}': ")
+		print("ID\tName\tDoB\tMark")	
+
+		for j in range(4):
+			print(CourseDict[CourseName][j],end="\t")
+		print("")
+
+
 Students=[]
-for i in range(StudentNum):
-	print(f"\nStudent {i+1}: ")
-
-	StudentId=int(input("Id: "))
-	StudentName=input("Name: ")
-	StudentDoB=input("Date of Birth: ")
-
-	Students.append([StudentId,StudentName,StudentDoB])
-
-CourseDict={}
-CourseNum= int(input("\nNumber of Courses: "))
 Courses=[]
-for i in range(CourseNum):
-	print(f"\nCourse {i+1}: ")
+CourseDict={}
 
-	CourseId=int(input("Id: "))
-	CourseName=input("Name: ")
+StudentNum= int(input("Number of Students: "))
+AddStudent(StudentNum, Students)
+CourseNum= int(input("\nNumber of Courses: "))
+AddCourse(CourseNum, Courses, CourseDict)
 
-	Courses.append([CourseId,CourseName])
-	CourseDict.update({str(CourseName):[]})
+SelectCourse(StudentNum, CourseNum, Students, CourseDict)
 
-for i in range(StudentNum):
-	print(f"\nSelect Course for Student {i+1}: ")
+DisplayList(Students, "Students")
+DisplayList(Courses, "Courses")
 
-	CourseName=input("Name: ")
-	CourseMark=int(input("Mark: "))
-	CourseDict[CourseName]+=[Students[i][0],Students[i][1],Students[i][2],CourseMark]	
-	#Key: CourseName, Value: Students[i] (Id, Name, DoB, Mark)
-
-print("\nList of Students: ")
-print("ID\tName\tDoB")
-for row in Students:
-	for element in row:
-		print(element,end="\t")
-	print("")
-
-print("\nList of Courses: ")
-print("ID\tName")
-for row in Courses:
-	for element in row:
-		print(element,end="\t")
-	print("")
-
-for i in range(CourseNum):
-	print(f"\nCourse '{Courses[i][1]}': ")
-	print("ID\tName\tDoB\tMark")
-	for j in range(4):
-		print(CourseDict[Courses[i][1]][j],end="\t")
-	print("")
+DisplayStudentInCourses(CourseNum, Courses, CourseDict)
