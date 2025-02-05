463099a02f41d7a9fafb69a5b11d0f843e445535
NguyenManhKhanhAn <annmk.23bi14003@usth.edu.vn>
2025-02-05 15:22:41 +0700

    now creates output files

--- pw5/input.py
+++ pw5/input.py
@@ -1,27 +1,23 @@
 import math
 import numpy as np
 
+
 class Info:
 	def __init__(self):
-		self.__id= int(input("\nID: "))
-		self.__name= input("Name: ")
-
-	def list(self):
-		print(f"{self.__id}\t{self.__name}")
-
+		self._id= int(input("\nID: "))
+		self._name= input("Name: ")
 	def get_id(self):
-		return self.__id
+		return self._id
 	def get_name(self):
-		return self.__name
+		return self._name
 
 class Course(Info):
 	def __init__(self):
 		super().__init__()
 		self.__credit= int(input("Credit: "))
-
-	def list(key, value):
-		print(f"{key}\t{value[0]}\t{value[1]}")
-
+		fCourse = open("courses.txt", "a")
+		fCourse.write(f"{self._id}\t{self._name}\t{self.__credit}\n")
+		fCourse.close()
 	def get_credit(self):
 		return self.__credit
 
@@ -33,7 +29,28 @@ class Student(Info):
 		self.__credit=[]
 		self.__mark=[]
 		self.__gpa=0
+		fStudent = open("students.txt", "a")
+		fStudent.write(f"{self._id}\t{self._name}\t{self.__dob}\n")
+		fStudent.close()
 
+	def selectCourse(self, Courses):
+		CourseID=int(input("Course ID: "))
+		self.__course.append(CourseID)
+		self.__credit.append(Courses[CourseID][1])
+		self.__mark.append(int(input("Mark: ")))
+		self.calculate_GPA()
+		fMark = open("marks.txt", "a")
+		fMark.write(f"{self._id}\t{self._name}\t{self.__course}\t{self.__credit}\t{self.__mark}{self.__gpa}\n")
+		fMark.close()
+
+	def calculate_GPA(self):
+		total_credit= sum(self.__credit)
+		if total_credit == 0:
+			self.__gpa= 0
+		else:
+			weighted_marks= np.array(self.__mark)*np.array(self.__credit) 		#Equal array of products
+			self.__gpa= math.floor(sum(weighted_marks)*10/total_credit)/10.0	#Round down to 1-digit decimal place
+	
 	def get_dob(self):
 		return self.__dob
 	def get_course(self):
@@ -44,21 +61,3 @@ class Student(Info):
 		return self.__mark
 	def get_gpa(self):
 		return self.__gpa
-	
-	def selectCourse(self, Courses):
-		CourseID=int(input("Course ID: "))
-		self.__course.append(CourseID)
-		self.__credit.append(Courses[CourseID][1])
-		self.__mark.append(int(input("Mark: ")))
-		self.calculate_GPA()
-
-	def calculate_GPA(self):
-		total_credit= sum(self.__credit)
-		if total_credit == 0:
-			self.__gpa= 0
-		else:
-			weighted_marks= np.array(self.__mark)*np.array(self.__credit) 		#Equal array of products
-			self.__gpa= math.floor(sum(weighted_marks)*10/total_credit)/10.0	#Round down to 1-digit decimal place
-
-	def list(self):
-		print(f"{self.__id}\t{self.__name}\t{self.__dob}\t{self.__credit}")
--- pw5/main.py
+++ pw5/main.py
@@ -7,6 +7,7 @@ if __name__=="__main__":
 
 	while True:
 		print ("\nSelect one:")
+		print ("0. Clear Database")
 		print ("1. Add Student")
 		print ("2. Add Course")
 		print ("3. Select Course")
@@ -14,27 +15,40 @@ if __name__=="__main__":
 		print ("5. Display Students")
 		print ("6. Display Students in a Course")
 		print ("7. Exit\n")
-		action= int(input())
+		action= input()
 		match action:
-			case 1: #Add Student
+			case "0":
+				fStudent = open("students.txt","w")
+				fCourse = open("courses.txt","w")
+				fMark = open("marks.txt","w")
+
+				fStudent.write("ID\tName\tDoB\n")
+				fCourse.write("ID\tName\tCredits\n")
+				fMark.write("ID\tName\tCourses\tCredits\tMarks\tGPA\n")
+
+				fStudent.close()
+				fCourse.close()
+				fMark.close()
+				print("[Database Cleared]")
+			case "1": #Add Student
 				temp=inp.Student()
 				Students.append(temp)
-			case 2: #Add Course
+			case "2": #Add Course
 				temp=inp.Course()
 				Courses.update({int(temp.get_id()):[temp.get_name(), temp.get_credit()]})
-			case 3: #Select Course
+			case "3": #Select Course
 				StID= int(input("\nSelect Course for Student ID: "))
 				for obj in Students:
 					if obj.get_id()==StID:
 						obj.selectCourse(Courses)
-			case 4: #Display Courses
+			case "4": #Display Courses
 				out.List_Course(Courses)
-			case 5: #Display Students
+			case "5": #Display Students
 				out.List_Student(Students)
-			case 6: #Display Student in a Course
+			case "6": #Display Student in a Course
 				out.List_Students_Course(Students)
-			case 7: 
-				print("Exiting...")
-				break
 			case _: 
-				print("Invalid Option") 
+				print("[Exiting...]")
+				break
+#			case _: 
+#				print("Invalid Option") 
